b005a5fc81fdd3bfb3c7943123fafda29bffb1c6
Пользователь <jaybalfort@gmail.com>
2023-02-08 08:34:11 -0600

    создал все необходимые папки и файлы

---
+++ src/Loader/LoaderLoading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoaderLoading = () => {
+  return <div>LoaderLoading</div>;
+};
+
+export default LoaderLoading;
---
+++ src/components/Product/ProductDetailsList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductDetailsList = () => {
+  return <div>ProductDetailsList</div>;
+};
+
+export default ProductDetailsList;
